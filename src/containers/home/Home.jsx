6244c43a3ae74dc95de02cef4be8e978c2fcab02
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-01-27 16:55:55 +0000

    added styling

--- src/containers/home/Home.jsx
+++ src/containers/home/Home.jsx
@@ -1,9 +1,13 @@
-
+import howl from "../../../src/assests/images/giphy-5.gif"
 const Home = () => {
   return (
     <>
+   
     <h1> Welcome to Booktok</h1>
+
     <h3> Track your book reviews here; the good, the bad and the mundane. </h3>
+    <img src={howl}/> 
+   
     </>
   )
 
