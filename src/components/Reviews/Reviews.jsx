44c966e335302e63964b1d8fb0df34f08e2c0d24
Ankita-Manmoandas <ankita.gandhim09@gmail.com>
2022-01-27 16:30:15 +0000

    added delete function to each review

--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,8 +1,10 @@
 import React from "react";
+import "./Reviews.scss"
 
 
 const Reviews = (props) => {
   const {
+    id,
     bookURL,
     title,
     author,
@@ -10,13 +12,41 @@ const Reviews = (props) => {
     review
   } = props.reviews
 
+  //delete button for each review
+  //link to delete request
+  const handleDelete = (event) => {
+    event.preventDefault()
+    fetch(`http://localhost:8080/reviews/${id}`, {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+     
+    })
+    .then((response) => response.json())
+    .then((json => console.log(json)))
+    .catch(err => console.log(err))
+    
+  }
+
+  
+
+
+
   return (
-    <div>
-      <img src={bookURL}/>
-      <h2>{title}</h2>
-      <p>Author: {author} </p>
-      <p> Rating: {score} </p>
-      <p> Your detailed review: {review} </p>
+    <div className="review">
+      <img className="review__cover"src={bookURL}/>
+      <h2 className="review__label">{title}</h2>
+      <br />
+      <p className="review__label">Author: 
+      <br/>
+      {author} </p>
+      <p className="review__label"> Rating: {score} </p>
+      <p className="review__label"> Your detailed review: 
+      <br/> {review} </p>
+      <button onClick={handleDelete}> delete</button>
+
+      
 
     </div>
   )
